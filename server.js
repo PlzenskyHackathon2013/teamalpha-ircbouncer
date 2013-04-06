e0a55960cd538e4f5a251b1a2523372dac415b90
Marek Jelen <marek@jelen.biz>
2013-04-06 19:11:31 +0200

    First version

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 #!/bin/env node
+
+var Bouncer = require('./lib/bouncer.js'),
+    IrcClient = require('./lib/irc_client'),
+    Web = require('./lib/web.js');
+
+var bouncer = new Bouncer.Bouncer();
+
+var irc_client = new IrcClient.IrcClient("second", "second.ph", "A-Team", {'channels': ["#PH"]});
+bouncer.addClient(irc_client);
+
+var web = new Web.Web(bouncer);
+web.start();
