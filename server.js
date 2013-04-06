8e8a003343780a382fcc5b9d275295ae002de549
Marek Jelen <marek@jelen.biz>
2013-04-06 19:30:23 +0200

    Deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var Bouncer = require('./lib/bouncer.js'),
 var bouncer = new Bouncer.Bouncer();
 
 var irc_client = new IrcClient.IrcClient("second", "second.ph", "A-Team", {'channels': ["#PH"]});
-bouncer.addClient(irc_client);
+bouncer.addClient("second", irc_client);
 
 var web = new Web.Web(bouncer);
 web.start();
